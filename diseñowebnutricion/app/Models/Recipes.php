4b6caeff025999452b4b6237111dbee65b7d55ff
itsMarlonFTP <118192989+itsMarlonFTP@users.noreply.github.com>
2025-03-15 15:15:20 -0600

    Creación de modelo de Recipes

---
+++ diseñowebnutricion/app/Models/Recipes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Recipes extends Model
+{
+    use HasFactory;
+    protected $table = "recipes";
+    protected $fillable = ["id","recipename","description","ingredients","instructions","calories","protein","carbs","fats","category","created_at","updated_at"];
+}
