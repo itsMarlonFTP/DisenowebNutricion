12a64133cca6e4a2fbd1dcbc78618c01718c1443
Roberto Ayala <reayalal69@gmail.com>
2025-03-15 20:38:33 -0600

    Allegies

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AllergyController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AllergyController::class, 'index']);
+Route::resource('allergies', AllergyController::class);
